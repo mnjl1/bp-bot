5719d4f37eca508f0c6bd70a18054b976f6c281c
mnjl1 <d.manzhula@gmail.com>
2026-02-01 22:07:00 +0200

    Complete db.py with all CRUD functions

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime, timedelta
 
 
 create_readings_table = """
@@ -24,10 +25,39 @@ def add_reading(user_id, systolic, diastolic, pulse=None, note=None):
     with sqlite3.connect('bp.db') as conn:
         cur = conn.cursor()
         cur.execute("""
-            INSERT INTO readings (user_id, systolic, diastolic, pulse, note)
+                    INSERT INTO readings (user_id, systolic, diastolic, pulse, note)
                     VALUES (?, ?, ?, ?, ?)
                     """,
-                    (user_id, systolic, diastolic, pulse, note))
+                    (user_id, systolic, diastolic, pulse, note)
+        )
+
+
+def get_last(user_id, n=5):
+    with sqlite3.connect('bp.db') as conn:
+        cur = conn.cursor()
+        cur.execute(
+                    """
+                    SELECT * FROM readings WHERE user_id = ?
+                    ORDER BY datetime DESC LIMIT ?
+                    """,
+                    (user_id, n)
+        )
+    return cur.fetchall()
+
+
+def get_avg(user_id, days=7):
+    with sqlite3.connect('bp.db') as conn:
+        last_days = datetime.now() - timedelta(days=days)
+        cur = conn.cursor()
+        cur.execute(
+            """
+            SELECT AVG(systolic), AVG(diastolic), AVG(pulse)
+            FROM readings
+            WHERE user_id = ? and datetime > ?
+            """,
+            (user_id, last_days)
+        )
+    return cur.fetchall()
 
 
 if __name__ == '__main__':
